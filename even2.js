d8a394b06442684bb7fbb3557e894707fe79aca0
shiv2137 <shiv21372137@gmail.com>
2022-11-16 18:42:51 +0530

    hello

---
+++ even2.js
@@ -0,0 +1,11 @@
+let number = prompt("Enter a number: ");
+
+//check if the number is even
+if(number % 2 == 0) {
+    alert("The number is even.");
+}
+
+// if the number is odd
+else {
+    alert("The number is odd.");
+}
